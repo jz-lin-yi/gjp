4b7bdc9b3253f51364976cb03cd2c5253c76d98d
linyi <linyimiss@gmail.com>
2019-09-26 10:35:24 +0800

    删除

--- gjp/src/com/itheima/gjp/app/MainApp.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.gjp.app;
-
-import com.itheima.gjp.view.MainView;
-
-public class MainApp {
-    public static void main(String[] args) {
-        new MainView().run();
-    }
-}
